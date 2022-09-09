f76a56dbc0dee3f90ca8376a8c1e741ebb178dd5
vanira0 <97962338+vanira0@users.noreply.github.com>
2022-09-10 01:42:46 +0530

    establish mongoDB connection

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
+const path = require('path');
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({ path: './config/.env' })
+
+connectDB();
+
+// Serve static files in Express
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.get('/',(req,res) => {
     res.send('Hello World');
 })
 
-app.listen(port, () => {
-    console.log(`Serving on port ${port}...`)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}...`)
 })
